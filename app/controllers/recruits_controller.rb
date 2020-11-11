47ce74434e4b40911bfa674bd9b7565cc0a46bf9
Yusuke Yasuo <yasuo.y@dely.jp>
2020-11-11 14:26:17 +0000

    日付検索

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -2,11 +2,13 @@ class RecruitsController < ApplicationController
   def index
     @keyword = params[:keyword].presence || ''
     @pref = params[:pref].presence || ''
+    @date = params[:date].presence || ''
     
     @recruits = Recruit.all
     
     @recruits = @recruits.where('comment like ?',"%#{params[:keyword]}%") if @keyword.present?
     @recruits = @recruits.where(prefecture: params[:pref]) if @pref.present?
+    @recruits = @recruits.where(start_at: @date.to_date.beginning_of_day..@date.to_date.end_of_day) if @date.present?
     
     @recruits = @recruits.order(id: :desc)
   end
