2433898607b4a990ec3b14be0eb115f6d53c52d7
masayuki <kik557473@gmail.com>
2020-11-18 13:48:26 +0000

    controller,index チームレベル修正

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,14 +1,16 @@
 class RecruitsController < ApplicationController
   def index
-    @keyword = params[:keyword].presence||''
-    @pref = params[:pref].presence||'' 
-    @date = params[:date].presence||'' 
-
+    @keyword = params[:keyword].presence || ''
+    @pref = params[:pref].presence || '' 
+    @date = params[:date].presence || '' 
+    @level_type = params[:level_type].presence || '' 
+    
     @recruits = Recruit.all
     
     @recruits = @recruits.where('comment like ?',"%#{params[:keyword]}%") if @keyword.present?
     @recruits = @recruits.where(prefecture: params[:pref]) if @pref.present?
     @recruits = @recruits.where(start_at: @date.to_date.beginning_of_day..@date.to_date.end_of_day) if @date.present?
+    @recruits = @recruits.where(level_type: params[:level_type]) if @level_type.present?
     
     @recruits = @recruits.order(id: :desc) 
   end
