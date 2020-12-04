98fb68d57eae26fdd0e92bf1c421e22ff3bb618a
masayuki <kik557473@gmail.com>
2020-12-04 15:27:49 +0000

    応募一覧ステータス追加

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,4 +4,9 @@ class RequestsController < ApplicationController
     #Request.create!(team_id: current_team.id, recruit_id: params[:recruit_id])
     redirect_to recruit_path(params[:recruit_id])
   end
+  
+  def update
+    # TODO update status
+    redirect_to recruit_path(params[:recruit_id])
+  end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,14 @@ class Request < ApplicationRecord
   belongs_to :team
   belongs_to :recruit
   enum status: { unconfirmed: 0, approval: 1, disapproval: 2 }
+  
+  DISPLAY_STATUS = {
+    unconfirmed: '未確認',
+    approval: '承認',
+    disapproval: '未承認'
+  }
+    
+  def display_status
+    DISPLAY_STATUS[status.to_sym]
+  end
 end
